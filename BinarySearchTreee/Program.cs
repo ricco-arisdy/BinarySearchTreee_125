de33c1ed11434e32a4d1840b98552481fc517b9d
riccoarisdy <riccoarisdy@gmail.com>
2022-12-26 17:19:37 +0700

    add consturctor for the node class to class node

--- BinarySearchTreee/Program.cs
+++ BinarySearchTreee/Program.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTreee
 {
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //consturctor for the node class
+    }
     internal class Program
     {
+      
         static void Main(string[] args)
         {
         }
